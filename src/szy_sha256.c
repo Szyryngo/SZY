6b096b098667c675830f585db09fb24efa7665ac
Szyryngo <robert.szyryngo@gmail.com>
2026-05-01 12:44:33 +0200

    Add license and copyright information
    Added licensing information and copyright notice to the top of the file.

--- src/szy_sha256.c
+++ src/szy_sha256.c
@@ -1,3 +1,26 @@
+/*
+ * SZY_v8 Compressor
+ * Copyright (C) 2026 Robert Szyryngo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---
+ * COMMERCIAL LICENSE
+ * If you want to use this software in proprietary, closed-source projects
+ * or in any other way that does not comply with the AGPLv3 license,
+ * a commercial license is required. Contact robert.szyryngo@gmail.com for pricing.
+ */
 #include "szy_sha256.h"
 #include <string.h>
 
